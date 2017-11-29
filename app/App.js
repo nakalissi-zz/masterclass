0a52405e9b4d0dafe335fba3d960e94bb2a34ce3
Daniel Calissi Nakatate <nakalissi@gmail.com>
2017-11-29 02:16:13 -0200

    Fix components

--- app/App.js
+++ app/App.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTab: 'active',
       header: [],
       label: [],
       data: []
@@ -21,6 +20,9 @@ export default class App extends Component {
           label: response.data.tabs,
           data: response.data.content
         });
+      })
+      .catch(err => {
+        alert(err);
       });
   }
   render() {
@@ -28,42 +30,36 @@ export default class App extends Component {
     const data = this.state.data;
     const label = this.state.label;
     return (
+      <div>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">{header.title}</a>
+          </div>
+        </div>
+      </nav>
       <main className="container">
       <div className="row">
-        <header><h1>{header.title}</h1></header>
         <Tabs items={this.state.label} />
         <div className="tab-content">
-          { Object.keys(data).map((x,i) => <Container id={x} key={x} status={label[x]} info={data[x]} /> )}
+          { Object.keys(data).map((x) => <Container id={x} key={x} status={label[x]} info={data[x]} /> )}
         </div>
       </div>
       </main> 
+      </div>
     ) 
   }
 }
 
 class Tabs extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeTab: ''
-    };
-  }
-  handleClick(e,tab){
-    console.log(e,tab);
-    this.setState(prev => ({
-      activeTab: tab
-    }));
-  }
   render(){
-    console.log(this.props.activeTab);
     const items = this.props.items;
     return (
       <ul className="nav nav-tabs">
       { Object.keys(items).map((x) => (
-        <li key={x} className={items[x].classes ? "active" : null} onClick={this.handleClick.bind(this,x)}>
+        <li key={x} className={items[x].classes}>
           <a data-toggle="tab" href={"#"+x}>{items[x].tab_name}  
-            <small className="badge label-primary">{items[x].total_items}</small>
+            <small className="badge badge-primary">{items[x].total_items}</small>
           </a>
         </li>
       ))
@@ -81,17 +77,14 @@ class Container extends React.Component {
     }
   }
   render(){
-    console.log(this.props.activeTab);
+
     let id = this.props.id;
     let info = this.props.info;
     let status = this.props.status;
-    console.log('info',info);
-    console.log(id, status);
+    // Populate media content 
     return (
-      <div>
-      <div id={id} className={"tab-pane fade" + (status.hasOwnProperty("classes") ? status.classes : "")}>
+      <div id={id} className={"tab-pane fade" + (status.hasOwnProperty("classes") ? " in active" : null)}>
         { Object.keys(info).map((x,i) => <Media title={info[x].title} img={info[x].image_url} description={info[x].description} /> )}
-      </div>
       </div>
     )
   }
